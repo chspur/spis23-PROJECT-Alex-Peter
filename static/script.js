1acbe85327b66fa658a614c519f491014c3e13df
Alexg014 <141669571+Alexg014@users.noreply.github.com>
2023-09-06 21:57:07 +0000

    Co-authored-by: petruswagnavian <petruswagnavian@users.noreply.github.com>

--- static/script.js
+++ static/script.js
@@ -3,16 +3,12 @@
 
 //make variables for the buttons and text bro bro
 let joverButton = document.querySelector(".joe-button");
-//let joverTextReference = document.getElementById("jover-text") not needed tbh
 let resultText = document.getElementById("result");
 let winLine = document.getElementsByClassName("win-line");
 let threexelement = document.getElementById("threebythree");
 let ninexelement = document.getElementById("ninebynine");
 let referenceThreeBox = document.querySelector(".tic-box");
 let referenceNineBox = document.querySelector(".ultimate-tic-box");
-// let drawquag = document.getElementById("draw-image");
-// let Lpaldean = document.getElementById("paldeanlost");
-// let Wpaldean = document.getElementById("paldeanwin")
 
 
 //query selector to search multiple elements
@@ -41,22 +37,41 @@ let firstTurn = true;
 let turnCount = 0; // counter to count the turns that have passed
 
 //resultText.style.visibility = 'hidden';
-// drawquag.style.visibility = 'hidden';
-// Lpaldean.style.visibility = 'hidden';
-// Wpaldean.style.visibility = 'hidden';
 
 ninexelement.addEventListener("click", setNine);
 function setNine() {
-    console.log('hit')
+    //turnCount = 0;
+    //resultText.innerHTML = "team QUAGSIRE's turn (X)";
     referenceThreeBox.style.display = 'none';
     referenceNineBox.style.display = 'grid';
+    startyManUltimate();
 }
 
-joverButton.addEventListener("click", startyMan);
+threexelement.addEventListener("click", setThree);
+function setThree() {
+    //turnCount = 0;
+    //resultText.innerHTML = "team QUAGSIRE's turn (X)";
+    referenceNineBox.style.display = 'none';
+    referenceThreeBox.style.display = 'grid';
+    startyMan();
+}
+
+//joverButton.addEventListener("click", startyMan);
+joverButton.addEventListener("click", () => {
+    if (referenceThreeBox.style.display == 'none') {
+        startyManUltimate();
+        console.log("slatt");
+    }
+    if (referenceThreeBox.style.display == 'grid') {
+        startyMan();
+        console.log("goog");
+    }
+});
+
 
 //what happens when a tic-tac-toe button gets clicked !!!11
 ticButton.forEach(clickyTic);
-ultimateTicButton.forEach(clickyTic);
+ultimateTicButton.forEach(clickyTicUltimate);
 
 //da click function
 function clickyTic(buttonTile) {
@@ -88,7 +103,6 @@ function checkDraw() { //what happens in a draw?
         enderMan();
         resultText.innerHTML = "Draw";
         //resultText.style.visibility = 'visible';
-        // drawquag.style.visibility = 'visible';
 
     }
 }
@@ -109,15 +123,11 @@ function checkWin() {
                 resultText.innerHTML = "team QUAGSIRE wins (X)";
                 //resultText.style.visibility = 'visible';
                 isWin = true;
-                // Lpaldean.style.visibility = 'visible';
-                //drawWinningLine();
             }
             if (first == "O") {
                 resultText.innerHTML = "team CLODSIRE wins (O)";
                 //resultText.style.visibility = 'visible';
                 isWin = true;
-                // Wpaldean.style.visibility = 'visible';
-                //drawWinningLine();
             }
             drawWinLine(i, first);
         }
@@ -160,12 +170,86 @@ function startyMan() { //show function for resets
     function startButtons(tileButtons) {
         tileButtons.disabled = false;
         tileButtons.innerText = "";
-        // resultText.style.visibility = 'hidden';
-        // drawquag.style.visibility = 'hidden';
-        // Lpaldean.style.visibility = 'hidden';
-        // Wpaldean.style.visibility = 'hidden';
     }
     for (let i = 0; i < 16; i++) {
         winLine[i].style.display = 'none';
     }
 }
+
+
+
+//copy paste functions for ultimate versions
+function clickyTicUltimate(buttonTile) {
+    buttonTile.addEventListener("click", () => { //when button is clicked, everything below is run
+        if (firstTurn == true) { //Quagsire's turn
+            firstTurn = false; //switch turns
+            resultText.innerHTML = "team CLODSIRE's turn (O)";
+            buttonTile.style.color = "lightskyblue";
+            buttonTile.innerText = "X"; //draw X
+            buttonTile.disabled = true; //make button not able to be clicked anymore
+        }
+        else { //Clodsire's turn
+            firstTurn = true; //switch turns
+            resultText.innerHTML = "team QUAGSIRE's turn (X)";
+            buttonTile.style.color = "saddlebrown";
+            buttonTile.innerText = "O"; //draw da O
+            buttonTile.disabled = true; //make button not able to be clicked anymore
+        }
+        turnCount += 1; //increase counter on every click
+        if (checkWinUltimate() == false) { //check for wins on every click
+            checkDraw(); //check for draws on every click
+        };
+    }
+    )
+}
+
+function checkDrawUltimate() { //what happens in a draw?
+    if (turnCount == 9) {
+        enderManUltimate();
+        resultText.innerHTML = "Draw";
+        //resultText.style.visibility = 'visible';
+
+    }
+}
+
+function checkWinUltimate() {
+    isWin = false;
+    for (let i = 0; i < threeToWin.length; i++) { //p to check everything
+        const p = threeToWin[i];
+        let [first, second, third] = [ //check combinations of 3
+            ultimateTicButton[p[0]].innerText, //innerText gets X or O
+            ultimateTicButton[p[1]].innerText,
+            ultimateTicButton[p[2]].innerText,
+        ];
+        if (first != "" && second != "" && third != "" && first == second && second == third) { 
+        //check if 3 slots are filled with something (X or O) and they are all equal to each other (all X or all Y)
+            enderManUltimate();
+            if (first == "X") {
+                resultText.innerHTML = "team QUAGSIRE wins (X)";
+                isWin = true;
+            }
+            if (first == "O") {
+                resultText.innerHTML = "team CLODSIRE wins (O)";
+                isWin = true;
+            }
+        }
+    }
+    return isWin;
+}
+function enderManUltimate() { //end function
+    ultimateTicButton.forEach(stopButtons);
+    function stopButtons(tileButtons) {
+        tileButtons.disabled = true;
+    }
+}
+function startyManUltimate() { //show function for resets
+    firstTurn = true;
+    turnCount = 0;
+    resultText.innerHTML = "team QUAGSIRE's turn (X)";
+    ultimateTicButton.forEach(startButtons);
+    function startButtons(tileButtons) {
+        tileButtons.disabled = false;
+        tileButtons.innerText = "";
+    }
+}
+
